c1d4c6d087135462e939bdfd3d1a9158fba6a71a
Jiwoon Won <wonjiwn@naver.com>
2020-03-30 23:48:39 +0900

    Set absolute path

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Text from '@components/atoms/Text';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Text />
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
---
+++ src/components/atoms/Text.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Text: React.FC = () => <p>hello</p>;
+
+export default Text;
